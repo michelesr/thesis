4e71eafcb6b6d2c19329eadb80478ed2fccd2655
Michele Sorcinelli <mikefender@cryptolab.net>
2015-08-17 17:42:57 +0200

    Renamed tesi.pdf -> thesis.pdf

--- scripts/compile_pdf.sh
+++ scripts/compile_pdf.sh
@@ -4,7 +4,7 @@ cd template
 
 # in order to parse generated .toc .aux. .lof .lot
 # pdflatex needs to be launched twice
-pdflatex tesi.tex 
-pdflatex tesi.tex
+pdflatex thesis.tex
+pdflatex thesis.tex
 
-mv tesi.pdf ../dist/
+mv thesis.pdf ../dist/
